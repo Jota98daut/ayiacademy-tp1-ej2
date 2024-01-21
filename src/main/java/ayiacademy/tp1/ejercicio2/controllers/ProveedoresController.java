364d91df4279a4a527771d80ccbd565eab104af5
Joel D'Autilio <joeldautilio@gmail.com>
2024-01-20 23:35:46 -0300

    borrar proveedor

--- src/main/java/ayiacademy/tp1/ejercicio2/controllers/ProveedoresController.java
+++ src/main/java/ayiacademy/tp1/ejercicio2/controllers/ProveedoresController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -33,6 +34,12 @@ public class ProveedoresController {
   @PostMapping("/proveedores/nuevo")
   public RedirectView postNuevoProveedor(Proveedor proveedor) {
     proveedoresService.save(proveedor);
+    return new RedirectView("/proveedores");
+  }
+
+  @GetMapping("/proveedores/eliminar/{idProveedor}")
+  public RedirectView getEliminarProveedor(@PathVariable int idProveedor) {
+    proveedoresService.delete(idProveedor);
     return new RedirectView("/proveedores");
   }
 }
