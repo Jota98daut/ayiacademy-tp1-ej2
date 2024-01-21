23cade8938212c53ad40fc5199095613fd5ac366
Joel D'Autilio <joeldautilio@gmail.com>
2024-01-20 23:32:04 -0300

    agregar proveedor

--- src/main/java/ayiacademy/tp1/ejercicio2/controllers/ProveedoresController.java
+++ src/main/java/ayiacademy/tp1/ejercicio2/controllers/ProveedoresController.java
@@ -1,25 +1,38 @@
 package ayiacademy.tp1.ejercicio2.controllers;
 
+import ayiacademy.tp1.ejercicio2.persistence.entities.Proveedor;
+import ayiacademy.tp1.ejercicio2.services.ProveedoresService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import ayiacademy.tp1.ejercicio2.services.ProveedoresService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ProveedoresController {
-    @Autowired
-    private ProveedoresService proveedoresService;
+  @Autowired private ProveedoresService proveedoresService;
 
-    @Value("${constants.proveedores-title}")
-    private String proveedoresTitle;
+  @Value("${constants.proveedores-title}")
+  private String proveedoresTitle;
 
-    @GetMapping("/proveedores")
-    public String getProveedores(Model model) {
-        model.addAttribute("titulo", proveedoresTitle);
-        model.addAttribute("proveedores", proveedoresService.getAll());
-        return "proveedores/index";
-    }
+  @GetMapping("/proveedores")
+  public String getProveedores(Model model) {
+    model.addAttribute("titulo", proveedoresTitle);
+    model.addAttribute("proveedores", proveedoresService.getAll());
+    return "proveedores/index";
+  }
+
+  @GetMapping("/proveedores/nuevo")
+  public String getNuevoProveedor(Model model) {
+    model.addAttribute("proveedor", new Proveedor());
+    return "proveedores/nuevo";
+  }
+
+  @PostMapping("/proveedores/nuevo")
+  public RedirectView postNuevoProveedor(Proveedor proveedor) {
+    proveedoresService.save(proveedor);
+    return new RedirectView("/proveedores");
+  }
 }
