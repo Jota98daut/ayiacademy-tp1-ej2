a3b0bfd73083073b9a7708d28d88c5bf82fa887f
Joel D'Autilio <joeldautilio@gmail.com>
2024-01-21 03:51:12 -0300

    agregar http

--- src/main/resources/scripts/insertar_datos.sql
+++ src/main/resources/scripts/insertar_datos.sql
@@ -6,8 +6,8 @@ INSERT INTO usuarios (nombre, password_usuario) VALUES
 ('Pedro Sánchez', 'privadaXYZ');
 
 INSERT INTO proveedores (nombre, telefono, direccion, email, sitio_web, estado) VALUES
-('ElectroHogar S.A.', '555-123-4567', 'Av. Tecnológica #789', 'info@electrohogar.com', 'www.electrohogar.com', 1),
-('ModaStyle', '222-987-6543', 'Calle de la Moda #321', 'ventas@modastyle.com', 'www.modastyle.com', 2),
-('Gourmet Foods', '888-555-9999', 'Plaza del Sabor #456', 'contacto@gourmetfoods.com', 'www.gourmetfoods.com', 3),
-('TechSoluciones', '777-444-3333', 'Calle de la Tecnología #654', 'info@techsoluciones.com', 'www.techsoluciones.com', 1),
-('Muebles Elegantes', '666-111-2222', 'Paseo de los Muebles #987', 'muebles@elegantes.com', 'www.muebleselegantes.com', 2);
+('ElectroHogar S.A.', '555-123-4567', 'Av. Tecnológica #789', 'info@electrohogar.com', 'http://www.electrohogar.com/', 1),
+('ModaStyle', '222-987-6543', 'Calle de la Moda #321', 'ventas@modastyle.com', 'http://www.modastyle.com/', 2),
+('Gourmet Foods', '888-555-9999', 'Plaza del Sabor #456', 'contacto@gourmetfoods.com', 'http://www.gourmetfoods.com/', 3),
+('TechSoluciones', '777-444-3333', 'Calle de la Tecnología #654', 'info@techsoluciones.com', 'http://www.techsoluciones.com/', 1),
+('Muebles Elegantes', '666-111-2222', 'Paseo de los Muebles #987', 'muebles@elegantes.com', 'http://www.muebleselegantes.com/', 2);
